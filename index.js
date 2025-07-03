052b3d9fd320b9190d6fe3c4857ae913c76ba00a
mitsuo <mitsuo@mitsuos-MacBook-Air.local>
2025-07-03 22:30:43 +0800

    random shapes unfinished

--- index.js
+++ index.js
@@ -1,22 +1,43 @@
-const { Engine, Render, Runner, World, Bodies } = Matter;
+const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } = Matter;
 
 const engine = Engine.create();
 const { world } = engine;
+
+const width = 800;
+const height = 600;
 
 const render = Render.create({
     element: document.body,
     engine: engine,
     options: {
-        width: 800,
-        height: 600,
+        width: width,
+        height: height,
+        wireframes: false,
     },
 });
 
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-const shape = Bodies.rectangle(200, 200, 50, 50, {
-    isStatic: true,
-});
+World.add(world, MouseConstraint.create(engine, {
+    mouse: Mouse.create(render.canvas),
+}));
 
-World.add(world, shape);
+//WALLS
+const walls = [
+    Bodies.rectangle(400, 0, 800, 40, { isStatic: true}), // TOP
+    Bodies.rectangle(0, 400, 40, 800, { isStatic: true}), // LEFT
+    Bodies.rectangle(800, 400, 40, 800, { isStatic: true}), // RIGHT
+    Bodies.rectangle(0, 600, 1600, 40, { isStatic: true}), // BOTTOM
+];
+World.add(world, walls);
+
+//RANDOM SHAPES
+for(let i = 0; i < 140; i++){
+    let positionX = Math.random() * width;
+    let positionY = Math.random() * height;
+    let randHeight = Math.random() * 100;
+    let randWidth = Math.random() * 100;
+    
+    World.add(world, Bodies.rectangle(positionX, positionY, randHeight, randWidth));
+}
